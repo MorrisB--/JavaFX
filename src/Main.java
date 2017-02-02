41af46eab22ae025dab062035e021befad9039a6
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-01 21:35:26 -0600

    Created starter template

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+	Stage window;
+	Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		window.setTitle("JavaFX Title");
+		button = new Button("Click!");
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 500, 500);
+
+		window.setScene(scene);
+		window.show();
+	}
 
 }
