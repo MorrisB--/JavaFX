3f489f03f344e0098439bba3dd067ea1feebaba2
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-07 17:58:09 -0600

    Created Main3

---
+++ src/Main3.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main3 extends Application {
+
+	Stage window;
+	Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		window = primaryStage;
+		window.setTitle("Alert");
+		
+		button = new Button("Click");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 500, 500);
+		window.setScene(scene);
+		window.show();
+
+	}
+
+}
