f1f9df1a38aa3899fa4d36ce0bafb34530277cff
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-07 18:07:02 -0600

    Created an alert box

--- src/Main3.java
+++ src/Main3.java
@@ -20,6 +20,7 @@ public class Main3 extends Application {
 		window.setTitle("Alert");
 		
 		button = new Button("Click");
+		button.setOnAction(e -> AlertBox.display("button title", "button message"));
 		
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
