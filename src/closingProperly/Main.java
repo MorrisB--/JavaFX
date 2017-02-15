99a3270e63ae98af17264076ead6f5638ee3e464
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-14 19:57:44 -0600

    Added closeProgram()

--- src/closingProperly/Main.java
+++ src/closingProperly/Main.java
@@ -1,5 +1,39 @@
 package closingProperly;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	Stage window;
+	Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		window = primaryStage;
+		window.setTitle("Closing the program");
+		button = new Button("Close program");
+		button.setOnAction(e -> closeProgram());
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 500, 500);
+		window.setScene(scene);
+		window.show();
+
+	}
+
+	private void closeProgram() {
+		System.out.println("Successfully saved!");
+		window.close();
+	}
 
 }
