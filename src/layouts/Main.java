329d9053a59a9340586903a1cfb28b68c79df34f
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-14 20:28:21 -0600

    Created a BorderPane

--- src/layouts/Main.java
+++ src/layouts/Main.java
@@ -2,7 +2,11 @@ package layouts;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,9 +22,25 @@ public class Main extends Application {
 
 		window = primaryStage;
 		window.setTitle("Layout");
+		
+		HBox topMenu = new HBox();
+		Button buttonA = new Button("buttonA");
+		Button buttonB = new Button("buttonB");
+		Button buttonC = new Button("buttonC");
+		topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+		
+		VBox leftMenu = new VBox();
+		Button button1 = new Button("button1");
+		Button button2 = new Button("button2");
+		Button button3 = new Button("button3");
+		leftMenu.getChildren().addAll(button1, button2, button3);
+		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(topMenu);
+		borderPane.setLeft(leftMenu);
 
-		StackPane layout = new StackPane();
-		Scene scene = new Scene(layout, 500, 500);
+		//StackPane layout = new StackPane();
+		Scene scene = new Scene(borderPane, 500, 500);
 		window.setScene(scene);
 		window.show();
 
