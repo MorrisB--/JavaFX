4ec6b849e16ce327e7a9ff34218732141d2bbf70
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-22 19:25:48 -0600

    Added Main.java

---
+++ src/gridPane/Main.java
@@ -0,0 +1,24 @@
+package gridPane;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+		
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+	
+		window.setTitle("Window title");
+		window.show();
+		
+	}
+
+}
