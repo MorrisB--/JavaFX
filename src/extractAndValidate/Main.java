16089d0bd87f394c291cbd075b0c161d247c258d
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-27 18:56:22 -0600

    Created main.java

---
+++ src/extractAndValidate/Main.java
@@ -0,0 +1,37 @@
+package extractAndValidate;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	Scene scene;
+	Button button;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+	
+		window.setTitle("Window title");
+		
+		TextField nameInput = new TextField();
+		button = new Button("Button");
+		
+		VBox layout = new VBox(10); // spacing the columns out 20 pixels
+		layout.setPadding(new Insets(20, 20, 20, 20));
+		layout.getChildren().addAll(nameInput, button);
+		
+		scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
+		window.show();
+		
+	}
+
+}
