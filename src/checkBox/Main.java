573a52610edd4ed93d750c64c0dc7bafeeb86b85
MorrisB-- <morris.ballenger@yahoo.com>
2017-02-28 20:33:40 -0600

    Creating CheckBoxes

--- src/checkBox/Main.java
+++ src/checkBox/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,16 +21,32 @@ public class Main extends Application{
 	
 		window.setTitle("Window title");
 		
+		CheckBox checkBox1 = new CheckBox("Beans");
+		CheckBox checkBox2 = new CheckBox("Bacon");
+
 		button = new Button("Send Order");
+		button.setOnAction(e -> handleCheckBoxOptions(checkBox1, checkBox2));
 		
 		VBox layout = new VBox(10); // spacing the columns out 20 pixels
 		layout.setPadding(new Insets(20, 20, 20, 20));
-		layout.getChildren().addAll(button);
+		layout.getChildren().addAll(checkBox1, checkBox2, button);
 		
 		scene = new Scene(layout, 300, 250);
 		window.setScene(scene);
 		window.show();
 		
+	}
+	
+	private void handleCheckBoxOptions(CheckBox box1, CheckBox box2) {
+		String message = "Boxes checked: ";
+		
+		if (box1.isSelected())
+			message += "Beans. ";
+		
+		if (box2.isSelected())
+			message += "Bacon. ";
+		
+		System.out.println(message);
 	}
 
 	
