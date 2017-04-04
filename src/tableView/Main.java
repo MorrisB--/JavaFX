614f81a60394e23347e178a99ab138c804610224
MorrisB-- <mo_mann@ymail.com>
2017-04-04 18:20:30 -0500

    Added Main.java template

--- src/tableView/Main.java
+++ src/tableView/Main.java
@@ -1,5 +1,25 @@
 package tableView;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage window) throws Exception {
+		window.setTitle("TableView");
+		
+		VBox vBox = new VBox();
+		vBox.getChildren().addAll();
+		
+		Scene scene = new Scene(vBox);
+		window.setScene(scene);
+		window.show();
+	}
 }
