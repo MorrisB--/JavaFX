9439137546f3a5c215cbfcd8dc56feef0faf0df9
MorrisB-- <mo_mann@ymail.com>
2017-04-05 17:00:12 -0500

    Added hBox to make table editable

--- src/tableView/Main.java
+++ src/tableView/Main.java
@@ -3,17 +3,21 @@ package tableView;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	TableView<Product> table;
-
+	TextField nameInput, priceInput, quantityInput;
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -38,12 +42,32 @@ public class Main extends Application {
 		quantityColumn.setMinWidth(100);
 		quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 		
+		nameInput = new TextField();
+		nameInput.setPromptText("Name");
+		nameInput.setMinWidth(100);
+		
+		priceInput = new TextField();
+		priceInput.setPromptText("Price");
+		priceInput.setMinWidth(100);
+		
+		quantityInput = new TextField();
+		quantityInput.setPromptText("Quantity");
+		quantityInput.setMinWidth(100);
+		
+		Button addButton = new Button("Add");
+		Button deleteButton = new Button("Delete");
+		
+		HBox hBox = new HBox();
+		hBox.setPadding(new Insets(10, 10, 10, 10));
+		hBox.setSpacing(10);
+		hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
+		
 		table = new TableView<>();
 		table.setItems(getProduct());
 		table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 		
 		VBox vBox = new VBox();
-		vBox.getChildren().addAll(table);
+		vBox.getChildren().addAll(table, hBox);
 		
 		Scene scene = new Scene(vBox);
 		window.setScene(scene);
