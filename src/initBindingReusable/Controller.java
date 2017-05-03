b3cba0dc27bf7a34891e831bf3bdf930fa98f96b
MorrisB-- <mo_mann@ymail.com>
2017-05-02 23:54:15 -0500

    Binding and reusing properties

---
+++ src/initBindingReusable/Controller.java
@@ -0,0 +1,15 @@
+package initBindingReusable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("Loading data");
+	}
+
+}
